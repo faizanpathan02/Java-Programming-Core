560a5bbae29dba6ade5c7dc92cf61e7fbcc0be5a
Faizan Pathan <pathanfaizan0202@gmail.com>
2026-02-03 19:12:58 +0530

    'Update'

--- src/com/Collection/Map/WorldExample/WorldController.java
+++ src/com/Collection/Map/WorldExample/WorldController.java
@@ -11,32 +11,47 @@ public class WorldController {
         World world = new World();
         Map<String, Map<String, Map<String, String>>> world1 = world.addCountry();
 
-        Set<String> keys = world1.keySet();
-        Iterator<String> itr = keys.iterator();
+//        Set<String> keys = world1.keySet();
+//        Iterator<String> itr = keys.iterator();
+//
+//        while (itr.hasNext()) {
+//            System.out.println(" ");
+//            String key = itr.next();
+//            System.out.println("Country Name : " + key);
+//
+//
+//            Map<String, Map<String, String>> country = world1.get(key);
+//            Iterator<String> itr2 = country.keySet().iterator();
+//
+//            while (itr2.hasNext()) {
+//                String key2 = itr2.next();
+//                System.out.println("State Name : " + key2);
+//
+//                Map<String, String> state = country.get(key2);
+//                Iterator<String> itr3 = state.keySet().iterator();
+//
+//                while (itr3.hasNext()) {
+//                    String key3 = itr3.next();
+//                    System.out.println("City Name : " + key3);
+//                }
+//            }
+//        }
 
-        while (itr.hasNext()) {
-            System.out.println(" ");
-            String key = itr.next();
-            System.out.println("Country Name : " + key);
 
+        for(String countryKey : world1.keySet()){
+            System.out.println();
+            System.out.println("Country Name : " + countryKey);
 
-            Map<String, Map<String, String>> country = world1.get(key);
-            Iterator<String> itr2 = country.keySet().iterator();
+            Map<String , Map<String , String>> country = world1.get(countryKey);
+            for(String stateKey : country.keySet()){
+                System.out.println("State Name : "+country.get(stateKey));
 
-            while (itr2.hasNext()) {
-                String key2 = itr2.next();
-                System.out.println("State Name : " + key2);
-
-                Map<String, String> state = country.get(key2);
-                Iterator<String> itr3 = state.keySet().iterator();
-
-                while (itr3.hasNext()) {
-                    String key3 = itr3.next();
-                    System.out.println("City Name : " + key3);
+                Map<String , String> statesKey = country.get(stateKey);
+                for (String cityKey : statesKey.keySet()) {
+                    System.out.println("City Name : "+statesKey.get(cityKey));
                 }
             }
         }
-
 
 
     }
